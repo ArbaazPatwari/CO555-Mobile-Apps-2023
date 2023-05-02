4da40fd3587da77fcda0f6f432b1e323037a2dc6
Arbaaz Patwari <arbaazpatwari@gmail.com>
2023-05-03 00:33:00 +0100

    starting login page authentication

--- app/src/main/java/com/example/chainmail_app/sign_in_page.java
+++ app/src/main/java/com/example/chainmail_app/sign_in_page.java
@@ -3,6 +3,8 @@ package com.example.chainmail_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class sign_in_page extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class sign_in_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_page);
+
+        TextView email = (TextView) findViewById(R.id.emailField);
+        TextView password = (TextView) findViewById(R.id.passwordField);
+
+        Button login = (Button) findViewById(R.id.loginButton);
+
+
     }
 }
