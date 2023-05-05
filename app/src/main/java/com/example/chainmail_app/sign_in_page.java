225af42a15c98480dbb1c3e2efbc8b6f95b92748
Arbaaz Patwari <arbaazpatwari@gmail.com>
2023-05-05 23:56:19 +0100

    trying to make login work

--- app/src/main/java/com/example/chainmail_app/sign_in_page.java
+++ app/src/main/java/com/example/chainmail_app/sign_in_page.java
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
 
 public class sign_in_page extends AppCompatActivity {
 
@@ -13,11 +14,40 @@ public class sign_in_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_page);
 
-        TextView email = (TextView) findViewById(R.id.emailField);
-        TextView password = (TextView) findViewById(R.id.passwordField);
+        EditText em1 = (EditText) findViewById(R.id.emailField);
+        EditText ps1 = (EditText) findViewById(R.id.passwordField);
+        Button btn1 = (Button) findViewById(R.id.loginButton);
 
-        Button login = (Button) findViewById(R.id.loginButton);
+        String exampleEmail = "admin@android.com";
+        String examplePassword = "admin";
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               System.out.println(em1.getText());
+               loginCheck();
+            }
+
+            public void loginCheck() {
+                boolean pass1 = false;
+
+                if (exampleEmail.equals(em1.getText()))
+                {
+                    System.out.println("Email correct");
+                    pass1 = true;
+                }
+                else if (examplePassword.equals(ps1.getText()))
+                {
+                    System.out.print("Password correct");
+                    pass1 = false;
+                }
+                else
+                {
+                    System.out.println("No information entered");
+                    pass1 = false;
+                }
+            }
+        });
 
     }
 }
